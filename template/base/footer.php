0f57c9545b7e9c351cf02ec6a6f36fa6e7ded302
Jone <abc@jone.xyz>
2018-12-24 10:16:39 +0800

    '修改模版底部链接'

--- template/base/footer.php
+++ template/base/footer.php
@@ -4,8 +4,8 @@
     <div class="footer-inner">
         <!-- Add your copyright text here -->
         <div class="footer-text">
-            &copy; 2017
-            <a href="../cn/about.html"><strong>WebStack</strong></a> design by <a href="http://viggoz.com" target="_blank"><strong>Viggo</strong></a>
+            &copy; 2018
+            <a href="/about.html"><strong>126工具箱</strong></a> 模版来自 <a href="https://github.com/WebStackPage/WebStackPage.github.io" target="_blank" rel="nofollow"><strong>WebStackPage</strong></a>
             <!--  - Purchase for only <strong>23$</strong> -->
         </div>
         <!-- Go to Top Link, just add rel="go-top" to any link to add this functionality -->
