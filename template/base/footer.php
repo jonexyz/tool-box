60e2854b6e31897f5de57aadc113afa0472e2ccf
Jone <abc@jone.xyz>
2018-12-24 14:04:39 +0800

    暂无

--- template/base/footer.php
+++ template/base/footer.php
@@ -5,7 +5,9 @@
         <!-- Add your copyright text here -->
         <div class="footer-text">
             &copy; 2018
-            <a href="/about.html"><strong>126工具箱</strong></a> 模版来自 <a href="https://github.com/WebStackPage/WebStackPage.github.io" target="_blank" rel="nofollow"><strong>WebStackPage</strong></a>
+            <a href="/about.html"><strong>126工具箱</strong></a> 模版来自 <a
+                    href="https://github.com/WebStackPage/WebStackPage.github.io" target="_blank"
+                    rel="nofollow"><strong>WebStackPage</strong></a>
             <!--  - Purchase for only <strong>23$</strong> -->
         </div>
         <!-- Go to Top Link, just add rel="go-top" to any link to add this functionality -->
